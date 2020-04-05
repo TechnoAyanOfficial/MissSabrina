cb19d70daf5fc04f440d38052cd08f884fc8b00b
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-04-05 20:15:05 +0530

    Update stickers.py

--- tg_bot/modules/stickers.py
+++ tg_bot/modules/stickers.py
@@ -206,7 +206,7 @@ def makepack_internal(msg, user, png_sticker, emoji, bot, packname, packnum):
         extra_version = ""
         if packnum > 0:
             extra_version = " " + str(packnum)
-        success = bot.create_new_sticker_set(user.id, packname, f"{name}'s Sticker Pack" + extra_version,
+        success = bot.create_new_sticker_set(user.id, packname, f"{name}'s Sticker Pack By @MissSabrina_Bot" + extra_version,
                                              png_sticker=png_sticker,
                                              emojis=emoji)
     except TelegramError as e:
