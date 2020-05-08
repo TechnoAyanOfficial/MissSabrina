49c605e75b6d4b3764c1bfdc0297af00bee772a1
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-05-08 10:57:49 +0530

    Update welcome.py

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -102,7 +102,7 @@ def new_member(bot: Bot, update: Update):
                     "I have been added to this chat - {} with \nID: <pre>{}</pre>".format(chat.title, chat.id),
                     parse_mode=ParseMode.HTML
                 )
-                update.effective_message.reply_text("Thanks for adding me Sweetheart,CHECK @ctrlsupport!")
+                update.effective_message.reply_text("Thanks for adding me Sweetheart,Join @SabrinaOfficial !")
 
             else:
                 # If welcome message is media, send with appropriate function
